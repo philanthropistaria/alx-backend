cbcf0c987b71303df14023fa1222cfaef2bc7aed
philanthropistaria <aatandoh56@gmail.com>
2024-07-04 14:47:52 +0000

    hii

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
 
-app = Flask(__name__)
-babel = Babel(app)
 
 class Config:
-LANGUAGES = ["en", "fr"]
-BABEL_DEFAULT_LOCALE = "en"
-BABEL_DEFAULT_TIMEZONE = "UTC"
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
 
 @app.route('/')
-def index():
-return render_template('1-index.html')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
+
 
 if __name__ == '__main__':
-app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=5000)
