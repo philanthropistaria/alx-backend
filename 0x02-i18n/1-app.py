5d64955fdcf40c1130982708665a7b6d31c2cd39
philanthropistaria <aatandoh56@gmail.com>
2024-07-04 14:46:17 +0000

    hii

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 babel = Babel(app)
 
 class Config:
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+LANGUAGES = ["en", "fr"]
+BABEL_DEFAULT_LOCALE = "en"
+BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('1-index.html')
+return render_template('1-index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
 
